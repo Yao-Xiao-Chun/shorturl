f40e438a6603de3f4aceea0a33ec966aea792861
Yao <maguohui7@foxmail.com>
2022-04-26 20:45:56 +0800

    init

---
+++ api/internal/handler/shortenhandler.go
@@ -0,0 +1,28 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/zeromicro/go-zero/rest/httpx"
+	"shorturl/api/internal/logic"
+	"shorturl/api/internal/svc"
+	"shorturl/api/internal/types"
+)
+
+func ShortenHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var req types.ShortenReq
+		if err := httpx.Parse(r, &req); err != nil {
+			httpx.Error(w, err)
+			return
+		}
+
+		l := logic.NewShortenLogic(r.Context(), svcCtx)
+		resp, err := l.Shorten(&req)
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.OkJson(w, resp)
+		}
+	}
+}
