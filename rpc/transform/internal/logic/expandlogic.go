d811d3f4052da8d383e3177fb1f999b5df763117
GH <maguohui@imagetru3d.com>
2022-04-27 18:21:13 +0800

    add:增加rpc服务调用

---
+++ rpc/transform/internal/logic/expandlogic.go
@@ -0,0 +1,31 @@
+package logic
+
+import (
+	"context"
+
+	"shorturl/rpc/transform/internal/svc"
+	"shorturl/rpc/transform/transform"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type ExpandLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+//测试多个代码，是否覆盖了
+func NewExpandLogic(ctx context.Context, svcCtx *svc.ServiceContext) *ExpandLogic {
+	return &ExpandLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *ExpandLogic) Expand(in *transform.ExpandReq) (*transform.ExpandResp, error) {
+	// todo: add your logic here and delete this line
+
+	return &transform.ExpandResp{}, nil
+}
